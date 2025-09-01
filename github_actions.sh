f81b5e19b376966a8210259350ca951f517bda7f
qr243vbi <uiguruigurovich@gmail.com>
2025-09-01 14:06:48 +0300

    update

--- github_actions.sh
+++ github_actions.sh
@@ -3,9 +3,9 @@ git clone https://github.com/huakim/repo-suse repo
 cd repo/pacman
 sudo bash -x ./aptbk.sh
 sudo bash -x ./aptat.sh
-apt-get install zypper
+sudo apt-get install zypper
 sudo bash -x ./aptat.sh
-zypper install osckit
+sudo zypper --gpg-auto-import-keys --non-interactive install osckit
 
 echo $HOME
 )
