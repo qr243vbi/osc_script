3426c1566e97de2b97f859ff41eef383f79b0838
qr243vbi <uiguruigurovich@gmail.com>
2025-09-01 17:44:27 +0300

    update

--- github_actions.sh
+++ github_actions.sh
@@ -22,7 +22,9 @@ sudo mount --bind $SCRIPT_DIR /opensuse/script
 cd $SCRIPT_DIR/extra/repo/pacman
 sudo bash -x ./aptbk.sh
 sudo bash -x ./aptat.sh
-sudo apt-get install zypper systemd-container
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install -y zypper systemd-container
 sudo bash -x ./aptat.sh
 sudo zypper --installroot=/opensuse --gpg-auto-import-keys --non-interactive install osckit coreutils bash sed obs-tools-zypper-pkg
 sudo systemd-nspawn -D /opensuse /bin/env "OBS_PROJECTS=${OBS_PROJECTS}" "OBS_USER=${OBS_USER}" "OBS_PASSWORD=${OBS_PASSWORD}" bash -x /script/github_actions.sh
